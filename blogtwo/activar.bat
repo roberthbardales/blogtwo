0eceb162fdf3d3aea7cb704a0b6e3f5d1ca9a713
user.name <roberthbardales@hotmail.com>
2024-03-07 19:34:39 -0500

    base

---
+++ blogtwo/activar.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k "cd /d D:\Proyectos GIT\env_blogtwo\Scripts & activate & cd /d D:\Proyectos GIT\blogtwo\blogtwo & python manage.py runserver 8000"
---
+++ blogtwo/blogtwo/settings/local.py
@@ -0,0 +1,34 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbblogtwo',
+        'USER': 'neunapp',
+        'PASSWORD': 'neunapp2020',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
